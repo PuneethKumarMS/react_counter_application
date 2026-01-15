8bd18b5bf2128c7fd285e9342c5c3018e7d627ab
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-01-16 05:05:57 +0530

    Refactor CounterApp component for cleaner code

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
 
 function CounterApp() {
-
   const [count, setCount] = useState(0);
-
   const [step, setStep] = useState(1);
-
   const increase = () => {
     setCount(count + step);
   };
@@ -23,7 +20,6 @@ function CounterApp() {
     <div className='counter-container'>
       <h1>React Counter</h1>
       <h2>Count:{count}</h2>
-
       <label >Step Value:</label>
 
       <input
@@ -33,12 +29,10 @@ function CounterApp() {
       min='1'
       />
       <br />
-
       <button onClick={increase}>Increase</button>
       <button onClick={decrease}>Decrease</button>
       <button onClick={reset}>Reset</button>
     </div>
   );
-
 }
 export default CounterApp;
